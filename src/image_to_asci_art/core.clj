0e0b221d990f7b01e82d02dae721d5c6f5c48453
Roman Schader <r.schader@reply.de>
2019-07-26 14:18:17 +0200

    first working solution

--- src/image_to_asci_art/core.clj
+++ src/image_to_asci_art/core.clj
@@ -1,42 +1,54 @@
 (ns image-to-asci-art.core
   (:require [clojure.java.io :as io])
   (:import
-    [java.lang Math]
-    [java.awt Image Color Robot]
-    [java.awt.image RenderedImage BufferedImageOp]
-    [javax.imageio ImageIO ImageWriter ImageWriteParam IIOImage]
-    [javax.imageio.stream FileImageOutputStream]))
+   [java.lang Math]
+   [java.awt Image Color Robot]
+   [java.awt.image RenderedImage BufferedImageOp]
+   [javax.imageio ImageIO ImageWriter ImageWriteParam IIOImage]
+   [javax.imageio.stream FileImageOutputStream]))
 
 (def ^:private pixel-mapping " .-+*wGHM#&%")
 
-(def image-source (ImageIO/read (io/resource "me.jpg")))
+(def image-source
+  (ImageIO/read (io/resource "smiley.jpg")))
 
-(def image-width (int (.getWidth image-source)))
+(def image-width
+  (int (.getWidth image-source)))
 
-(def image-height (int (.getHeight image-source)))
-
-(defn get-colors [^java.awt.image.BufferedImage img [x y] [w h]]
-  (for [x (range x (+ x w))
-        y (range y (+ y h))]
-    (let [c (Color. (.getRGB img x y))]
-      [(.getRed c) (.getGreen c) (.getBlue c)])))
+(def image-height
+  (int (.getHeight image-source)))
 
 (defn calc-brightness [color]
   (let [[red green blue] color]
     (Math/sqrt
-      (+
-       (* red red 0.241)
-       (* green green 0.691)
-       (* blue blue 0.068)))))
+     (+
+      (* red red 0.241)
+      (* green green 0.691)
+      (* blue blue 0.068)))))
 
 ;;var idx = brightness / 255 * (_pixels.Length - 1);
-(defn to-pixel-idx [arg] (/ (int arg) (* 255 (- (count pixel-mapping) 1))))
 
-(def pixel-color-values (get-colors image-source [0 0] [image-width image-height]))
+(defn- get-index [pixel-mapping idx]
+  (- (count pixel-mapping) (int (Math/round idx)) 1))
 
-(def pixel-brightness-values (map #(calc-brightness %) pixel-color-values))
+(defn- to-pixel-idx [brightness]
+  (let [dividend (* 255 (- (count pixel-mapping) 1))]
+    (/ brightness (/ dividend 100))))
 
-(def pixel-idx-values (map #(to-pixel-idx %) pixel-brightness-values))
+(defn- get-colors [^java.awt.image.BufferedImage img
+                   [x y]
+                   [w h]]
+  ;;(with-open [output (clojure.java.io/writer "Example.txt")]
+  (doseq [y (range y (+ y h))]
+    (do
+      (let [res
+            (map #(let [c (Color. (.getRGB img % y))
+                        rgb [(.getRed c) (.getGreen c) (.getBlue c)]
+                        brightness (calc-brightness rgb)
+                        idx (to-pixel-idx brightness)
+                        mapping-index (get-index pixel-mapping idx)
+                        asci-sign (nth pixel-mapping mapping-index)]
+                    asci-sign) (range x (+ x w)))]
+        (spit "Example.txt" (str (apply str res) "\n") :append true)))))
 
-(defn -main [& args] (print pixel-color-values ))
-
+(defn -main [& args] (get-colors image-source [0 0] [image-width image-height]))
