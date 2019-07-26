4b4523e37e5d4b339d6b870ec42787b142f17e4c
Roman Schader <r.schader@reply.de>
2019-07-26 15:15:19 +0200

    works better

--- src/image_to_asci_art/core.clj
+++ src/image_to_asci_art/core.clj
@@ -2,12 +2,14 @@
   (:require [clojure.java.io :as io])
   (:import
    [java.lang Math]
-   [java.awt Image Color Robot]
-   [java.awt.image RenderedImage BufferedImageOp]
-   [javax.imageio ImageIO ImageWriter ImageWriteParam IIOImage]
-   [javax.imageio.stream FileImageOutputStream]))
+   [java.awt Image Color]
+   [java.awt.image BufferedImage]
+   [javax.imageio ImageIO]))
 
-(def ^:private pixel-mapping " .-+*wGHM#&%")
+(def pixel-mapping " .-+*wGHM#&%")
+
+(def output-path
+  "Example.txt")
 
 (def image-source
   (ImageIO/read (io/resource "smiley.jpg")))
@@ -18,15 +20,13 @@
 (def image-height
   (int (.getHeight image-source)))
 
-(defn calc-brightness [color]
+(defn- calc-brightness [color]
   (let [[red green blue] color]
     (Math/sqrt
      (+
       (* red red 0.241)
       (* green green 0.691)
       (* blue blue 0.068)))))
-
-;;var idx = brightness / 255 * (_pixels.Length - 1);
 
 (defn- get-index [pixel-mapping idx]
   (- (count pixel-mapping) (int (Math/round idx)) 1))
@@ -35,20 +35,21 @@
   (let [dividend (* 255 (- (count pixel-mapping) 1))]
     (/ brightness (/ dividend 100))))
 
-(defn- get-colors [^java.awt.image.BufferedImage img
-                   [x y]
-                   [w h]]
-  ;;(with-open [output (clojure.java.io/writer "Example.txt")]
-  (doseq [y (range y (+ y h))]
-    (do
-      (let [res
-            (map #(let [c (Color. (.getRGB img % y))
-                        rgb [(.getRed c) (.getGreen c) (.getBlue c)]
-                        brightness (calc-brightness rgb)
-                        idx (to-pixel-idx brightness)
-                        mapping-index (get-index pixel-mapping idx)
-                        asci-sign (nth pixel-mapping mapping-index)]
-                    asci-sign) (range x (+ x w)))]
-        (spit "Example.txt" (str (apply str res) "\n") :append true)))))
+(defn- write-line-to-file [filepath, line]
+  (let [seq-line (seq line)]
+    (with-open [w (io/writer filepath :append true)]
+      (.write w (str (apply str seq-line) "\n")))))
 
-(defn -main [& args] (get-colors image-source [0 0] [image-width image-height]))
+(defn- write-image [^BufferedImage img
+                   [w h]]
+  (doseq [y (range 0 h)]
+    (do
+      (let [res (map #(let [c (Color. (.getRGB img % y))
+                            rgb [(.getRed c) (.getGreen c) (.getBlue c)]
+                            brightness (calc-brightness rgb)
+                            idx (to-pixel-idx brightness)
+                            mapping-index (get-index pixel-mapping idx)
+                            asci-sign (nth pixel-mapping mapping-index)] asci-sign) (range 0 w))]
+        (write-line-to-file output-path res)))))
+
+(defn -main [& args] (write-image image-source [image-width image-height]))
